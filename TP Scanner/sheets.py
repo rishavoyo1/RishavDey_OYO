a16d0c86225537fe0878a3eea48dfcfc62cf7713
rishavoyo1 <rishav.dey1@oyorooms.com>
2026-07-07 23:45:34 +0530

    Update sheets.py

--- TP Scanner/sheets.py
+++ TP Scanner/sheets.py
@@ -1,42 +1,29 @@
-import oyoms as om
-import pandas as pd
+import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
 from datetime import datetime
 
-# =====================================
-# CONFIG
-# =====================================
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+]
 
-USER_EMAIL = "rishav.dey1@oyorooms.com"
-
-EXCEL_LINK = "https://oyoenterprise-my.sharepoint.com/:x:/r/personal/rishav_dey1_oyorooms_com/_layouts/15/Doc.aspx?sourcedoc=%7B8B74D613-B7E8-4720-8335-51AC2BE3F082%7D&file=Book.xlsx&action=editnew&mobileredirect=true&wdPreviousSession=c33c55c4-b724-7da3-4540-f8c22346c63d&wdNewAndOpenCt=1783371192173&wdo=4&wdOrigin=wacFileNew&wdPreviousCorrelation=c48526b1-3ae2-4f58-84a5-ea0762c335d2&wdnd=1"
-
-SHEET_NAME = "Dump"
-
-# =====================================
-# CONNECT
-# =====================================
-
-wb = om.WorkbookClient(
-    USER_EMAIL,
-    EXCEL_LINK
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=SCOPES,
 )
 
-# =====================================
-# FUNCTION
-# =====================================
+client = gspread.authorize(creds)
+
+sheet = client.open("TP Scanner").worksheet("Dump")
+
 
 def append_submission(booking, name, email, trustpilot_link):
 
-    df = pd.DataFrame([{
-        "Timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "Booking ID": booking,
-        "Guest Name": name,
-        "Email": email,
-        "Trustpilot Link": trustpilot_link
-    }])
-
-    wb.append_data(
-        sheet_name=SHEET_NAME,
-        df=df,
-        include_header=False
-    )
+    sheet.append_row([
+        datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        booking,
+        name,
+        email,
+        trustpilot_link
+    ])
